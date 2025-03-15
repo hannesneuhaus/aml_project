4f57b657daf5142d677b42b88e437bead24d26df
Hannes Neuhaus <hneuhaus@phoenixcontact.com>
2025-03-15 13:32:45 +0100

    refactor: renamed notebook feat: added tags to the model feat: added requirements.txt change: updated tehe readme

--- README.md
+++ README.md
@@ -6,4 +6,24 @@ This is written using python 3.11. To ensure that this works properly, please en
 Install all necessary requirements using the requirements.txt file using the following command
 pip install -r ./requirements.txt
 
-#
+## Usage
+
+To receive correct predictions is necessary to use the exact name of the movie as it is used in the training data. The easiest way currently is to copy
+the name of the movie from the movies.csv file.
+To interact with the model there are two possible ways. It is possible to give the model movies as a list (compare recommender_notebook, cell six), and then run the method.
+But there is also a terminal controller that allowes the user to add movies via a simple terminal menu.
+Running the scripy.py will automatically prepare the data, train the model and start the terminal menu. This will take time
+
+### Licensing
+
+Neither the University of Minnesota nor any of the researchers involved can guarantee the correctness of the data, its suitability for any particular purpose, or the validity of results based on the use of the data set. The data set may be used for any research purposes under the following conditions:
+
+- The user may not state or imply any endorsement from the University of Minnesota or the GroupLens Research Group.
+- The user must acknowledge the use of the data set in publications resulting from the use of the data set (see below for citation information).
+- The user may redistribute the data set, including transformations, so long as it is distributed under these same license conditions.
+- The user may not use this information for any commercial or revenue-bearing purposes without first obtaining permission from a faculty member of the GroupLens Research Project at the University of Minnesota.
+- The executable software scripts are provided "as is" without warranty of any kind, either expressed or implied, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose. The entire risk as to the quality and performance of them is with you. Should the program prove defective, you assume the cost of all necessary servicing, repair or correction.
+
+In no event shall the University of Minnesota, its affiliates or employees be liable to you for any damages arising out of the use or inability to use these programs (including but not limited to loss of data or data being rendered inaccurate).
+
+If you have any further questions or comments, please email <grouplens-info@umn.edu>
--- script.py
+++ script.py
@@ -1,39 +1,44 @@
-import sys
 import pandas as pd
 import numpy as np
 import tensorflow as tf
 from typing import Dict, Text
 import tensorflow_recommenders as tfrs
+import sys
 from IPython.display import clear_output
-
 
 # This is the small dataset
 ds_mov = 'Data/ml-latest-small/movies.csv'
 ds_rat = 'Data/ml-latest-small/ratings.csv'
 ds_tags = 'Data/ml-latest-small/tags.csv'
 
-# This is the entire dataset, because of it's size this will take a while to load
-#ds_mov = 'Data/ml-latest/movies.csv'
-#ds_rat = 'Data/ml-latest/ratings.csv'
-#ds_tags = 'Data/ml-latest/tags.csv'
+# This is the entire dataset, because of it's size this will take a long while to train the model
+# ds_mov = 'Data/ml-latest/movies.csv'
+# ds_rat = 'Data/ml-latest/ratings.csv'
+# ds_tags = 'Data/ml-latest/tags.csv'
 
 
 df_mov = pd.read_csv(ds_mov)
 df_rat = pd.read_csv(ds_rat)
 df_tags = pd.read_csv(ds_tags)
-df_rat = pd.merge(df_rat, df_mov, on="movieId", how="left")
-df_mov["movieId"] = df_mov["movieId"].astype(str)
-df_rat["userId"] = df_rat["userId"].astype(str)
-df_rat["movieId"] = df_rat["movieId"].astype(str)
+df_model = pd.merge(df_rat, df_mov, on='movieId', how='left')
+df_model = pd.merge(df_model, df_tags, on=['movieId', 'userId'], how='left')
+df_mov['movieId'] = df_mov['movieId'].astype(str)
+df_model['userId'] = df_model['userId'].astype(str)
+df_model['movieId'] = df_model['movieId'].astype(str)
+df_model['tag'] = df_model['tag'].fillna('').astype(str)
+df_model = df_model.drop(columns=['timestamp_x', 'timestamp_y'])
 
-mov_feat = {key: tf.constant(value) for key, value in df_mov.to_dict(orient="list").items()}
+
+mov_feat = {key: tf.constant(value) for key, value in df_mov.to_dict(orient='list').items()}
 movies = tf.data.Dataset.from_tensor_slices(mov_feat)
 
-rat_feat = {key: tf.constant(value) for key, value in df_rat.to_dict(orient="list").items()}
+rat_feat = {key: tf.constant(value) for key, value in df_model.to_dict(orient='list').items()}
 ratings = tf.data.Dataset.from_tensor_slices(rat_feat)
 
 movies = movies.map(lambda x: x['title'])
-ratings = ratings.map(lambda x: {"userId": x["userId"], "title": x["title"], 'rating': x['rating']})
+model_data = ratings.map(lambda x: {'userId': x['userId'], 'title': x['title'], 'rating': x['rating'], 'tag': x['tag']})
+shuffled = model_data.shuffle(100_000, seed=42, reshuffle_each_iteration=False)
+
 
 class UserModel(tf.keras.Model):
     def __init__(self):
@@ -42,20 +47,32 @@ class UserModel(tf.keras.Model):
             tf.keras.layers.StringLookup(vocabulary=unique_user_ids.astype(str), mask_token=None),
             tf.keras.layers.Embedding(len(unique_user_ids) + 1, embedding)
         ])
-        
+        self.tag_embedding = tf.keras.Sequential([
+            tf.keras.layers.StringLookup(vocabulary=unique_tags.astype(str), mask_token=None),
+            tf.keras.layers.Embedding(len(unique_tags) + 1, embedding)
+        ])
+                
     def call(self, inputs):
-        return self.user_embedding(inputs)
+        if isinstance(inputs, dict):
+            user_embedding = self.user_embedding(inputs['userId'])
+            tag_embedding = self.tag_embedding(inputs['tag'])
+            return tf.concat([user_embedding, tag_embedding], axis=1)
+        else:
+            return self.user_embedding(inputs)
+
 
 class MovieModel(tf.keras.Model):
     def __init__(self):
         super().__init__()
         self.movie_embedding = tf.keras.Sequential([
             tf.keras.layers.StringLookup(vocabulary=unique_movie_titles.astype(str), mask_token=None),
-            tf.keras.layers.Embedding(len(unique_movie_titles) + 1, embedding)
+            tf.keras.layers.Embedding(len(unique_movie_titles) + 1, embedding*2)
         ])
+        
         
     def call(self, inputs):
         return self.movie_embedding(inputs)
+    
 
 class RecommenderModel(tfrs.Model):
     def __init__(self):
@@ -67,75 +84,68 @@ class RecommenderModel(tfrs.Model):
         ))
         
     def compute_loss(self, features, training=False):
-        print(features)
-        user_embeddings = self.user_model(features["userId"])
-        movie_embeddings = self.movie_model(features["title"])
+
+        user_embeddings = self.user_model({'userId': features['userId'], 'tag': features['tag']})  
+        movie_embeddings = self.movie_model(features['title'])
+
         return self.task(user_embeddings, movie_embeddings)
 
-embedding = 64
-unique_user_ids = np.unique([x.numpy().decode("utf-8") for x in ratings.map(lambda x: x["userId"])])
-unique_movie_titles = np.unique([x.numpy().decode("utf-8") for x in movies])
+embedding = 32
 
+unique_user_ids = np.unique([x.numpy().decode('utf-8') for x in model_data.map(lambda x: x['userId'])])
+unique_movie_titles = np.unique([x.numpy().decode('utf-8') for x in movies])
+
+unique_tags = np.unique([
+    x.numpy().decode('utf-8') for x in model_data.map(lambda x: x['tag']) if x.numpy().decode('utf-8') != ''
+])
 
 model = RecommenderModel()
 model.compile(optimizer=tf.keras.optimizers.Adagrad(learning_rate=0.1))
 
+train = shuffled.take(80_000)
+test = shuffled.skip(80_000).take(20_000)
 
-cached_train = ratings.shuffle(100_000).batch(8192).cache()
-model.fit(cached_train, epochs=5)
+cached_train = train.shuffle(100_000).batch(8192).cache()
+cached_test = test.batch(4096).cache()
 
-def recommend_movies(model, movie_list, movie_dataset, top_k=3):
-    movie_embeddings = model.movie_model(tf.constant(movie_list))
-    
-    all_movie_titles = [x.numpy().decode("utf-8") for x in movie_dataset]
-    all_movie_embeddings = model.movie_model(tf.constant(all_movie_titles))
-    
-    scores = tf.linalg.matmul(movie_embeddings, all_movie_embeddings, transpose_b=True)
-    avg_scores = tf.reduce_mean(scores, axis=0) 
-    
-    top_indices = tf.argsort(avg_scores, direction='DESCENDING').numpy()
-    recommended_movies = [all_movie_titles[i] for i in top_indices if all_movie_titles[i] not in movie_list][:top_k]
-    
-    return recommended_movies
-
+model.fit(cached_train, epochs=10)
 
 def terminal_controller():
     list_of_movies = []
     while True:
-        print ("Welcome to the movie recommender system!")
-        print ("Please enter what you want to do:")
-        print ("1. Add a movie you like")
-        print ("2. Get movie recommendations")
-        print ("3. List your movies")
-        print ("4. Clear screen")
-        print ("5. Exit")
-        print ("")
+        print ('Welcome to the movie recommender system!')
+        print ('Please enter what you want to do:')
+        print ('1. Add a movie you like')
+        print ('2. Get movie recommendations')
+        print ('3. List your movies')
+        print ('4. Clear screen')
+        print ('5. Exit')
+        print ('')
         sys.stdout.flush() 
-        user_input = input("Enter your choice: ")
-        if user_input == "1":
-            movie = input("Enter the name of the movie you like: ")
+        user_input = input('Enter your choice: ')
+        if user_input == '1':
+            movie = input('Enter the name of the movie you like: ')
             list_of_movies.append(movie)
-            print (f"Added {movie} to your list of liked movies.")
-            print ("")
-        elif user_input == "2":
+            print (f'Added {movie} to your list of liked movies.')
+            print ('')
+        elif user_input == '2':
             recommendations = recommend_movies(model, list_of_movies, movies, top_k=5)
-            print ("We recommend the following movies:")
+            print ('We recommend the following movies:')
             for i, movie in enumerate(recommendations):
-                print (f"{i+1}. {movie}")
-            print ("")
-        elif user_input == "3":
-            print ("You have liked the following movies:")
+                print (f'{i+1}. {movie}')
+            print ('')
+        elif user_input == '3':
+            print ('You have liked the following movies:')
             for i, movie in enumerate(list_of_movies):
-                print (f"{i+1}. {movie}")
-            print ("")
-        elif user_input == "4":
+                print (f'{i+1}. {movie}')
+            print ('')
+        elif user_input == '4':
             clear_output()
-        elif user_input == "5":
-            print ("Thank you for using the movie recommender system!")
+        elif user_input == '5':
+            print ('Thank you for using the movie recommender system!')
             break
         else:
-            print ("Invalid input. Please try again.")
-            print ("")
-
+            print ('Invalid input. Please try again.')
+            print ('')
 
 terminal_controller()
